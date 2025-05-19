d08341d0412194408941f023192f89f50f70ba68
hari73966 <154949747+hari73966@users.noreply.github.com>
2025-05-19 18:07:32 +0530

    Add files via upload

---
+++ Expense_Tracker/main.dart
@@ -0,0 +1,12 @@
+import 'package:expense_tracker/models/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Expenses(),
+      ),
+    ),
+  );
+}
