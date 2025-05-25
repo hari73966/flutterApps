095c75c871226ea08c980bdcb8109bf5a59e70c3
hari73966 <154949747+hari73966@users.noreply.github.com>
2025-05-25 22:19:12 +0530

    Add files via upload

---
+++ meals/widgets/meals_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MealItem extends  extends StatelessWidget {
+  const MealItem({super.key,required this.meal,});
+  final Meal meal;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      
+    );
+  }
+}
